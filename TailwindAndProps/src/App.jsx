302b8a00fe3f4ac5f7259927aaae9ee90acbc6ac
Sujal Patel <sdpatel7122005@gmail.com>
2026-01-11 15:17:47 +0530

    "project 2"

---
+++ TailwindAndProps/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello Tailwind CSS!</h1>
+      <Card cardTitle="Tailwind Card 1" cardDesc="A clean card component built using Tailwind CSS in React." />
+      <Card cardTitle="Tailwind Card 2" cardDesc="Another clean card component built using Tailwind CSS in React." />
+      <Card cardTitle="Tailwind Card 3" cardDesc="Yet another clean card component built using Tailwind CSS in React." />
+    </>
+  )
+}
+
+export default App
